f7f6f1bb6f433fe7f211255f2c8bec0478e3b293
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2015-06-21 00:48:15 +0200

    Make test site a tad more complicated

--- web/index.php
+++ web/index.php
@@ -1,3 +1,27 @@
 <?php
 
-phpinfo();
+
+require '../vendor/autoload.php';
+
+$client = new GuzzleHttp\Client();
+$cache = new Doctrine\Common\Cache\FilesystemCache(__DIR__ . '/../storage/cache/doctrine');
+
+$twig = new Twig_Environment(
+    new Twig_Loader_Filesystem(__DIR__ . '/../templates'),
+    [
+        'cache' => __DIR__ . '/../storage/cache/twig',
+    ]);
+
+if ($cache->contains('posts')) {
+    $recentPosts = $cache->fetch('posts');
+} else {
+    $response = $client->get('http://www.onemorething.nl/api/get_recent_posts');
+    $body = json_decode($response->getBody());
+    $recentPosts = $body->posts;
+    foreach ($recentPosts as &$article) {
+        $article->title = html_entity_decode($article->title);
+    }
+    $cache->save('posts', $recentPosts);
+}
+
+echo $twig->render('home.twig', ['articles' => $recentPosts]);
